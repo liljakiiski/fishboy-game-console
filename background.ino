5849b520397430a80bbee09ba20799ce94943504
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2023-07-01 19:50:17 -0700

    being struct

--- background.ino
+++ background.ino
@@ -1,1 +1,8 @@
-void paint_bg
+void paint_bg(){
+  tft.fillScreen(BG);
+}
+
+void draw_fish(){
+  tft.drawPixel(50, 50, BAIT);
+  tft.drawRect(50, 50, 100, 100, FISH);
+}
--- fishboy.ino
+++ fishboy.ino
@@ -1,9 +1,30 @@
 #include <TFT_HX8357.h>
 
-FT_HX8357 tft = TFT_HX8357();
+//Constants
+//unsigned int colour = red << 11; // Colour order is RGB 5+6+5 bits each
+#define BG TFT_CYAN
+#define FISH TFT_BLUE
+#define BAIT TFT_RED
+
+struct Being {
+  int width;
+  int height;
+  int x;
+  int y;
+  unsigned int color;
+};
+
+TFT_HX8357 tft = TFT_HX8357();
+
+Being fish;
 
 void setup(){
-
+  fish = {100, 100, 20, 20, TFT_BLUE};
+  tft.init();
+  Serial.begin(9600);
+  Serial.println(fish.width);
+  paint_bg();
+  draw_fish();
 }
 
 void loop(){
